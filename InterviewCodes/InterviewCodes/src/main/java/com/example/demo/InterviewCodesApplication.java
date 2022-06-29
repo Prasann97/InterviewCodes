a40ba4a440ca52de2c20e45130f86d5e7114a160
Prasann97 <63595583+Prasann97@users.noreply.github.com>
2022-06-29 17:55:39 +0530

    Add files via upload

---
+++ InterviewCodes/InterviewCodes/src/main/java/com/example/demo/InterviewCodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCodesApplication.class, args);
+	}
+
+}
